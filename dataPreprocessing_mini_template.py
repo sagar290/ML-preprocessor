0d58b4645b467fd40493df8705e98eaef8f1057c
sagar290 <pcl1339@gmail.com>
2018-11-18 21:36:56 +0600

    Preprocessing template added

---
+++ dataPreprocessing_mini_template.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 18 20:23:38 2018
+
+@author: SAGAR
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 3].values
+
+#Splitting dataset into trainnig set and test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
+
+#Feature Scaling (Optional)
+"""
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
